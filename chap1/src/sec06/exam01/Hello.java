8a102562d72e094f28039e1768dd9eadb10ac40c
yuhn126 <yuhn126@naver.com>
2018-12-17 21:44:40 +0900

    chap1
    test

---
+++ chap1/src/sec06/exam01/Hello.java
@@ -0,0 +1,12 @@
+package sec06.exam01;
+
+public class Hello {
+
+ public static void main (String[] args) {
+	 int value = 10;
+	 
+	 int result = value + 10;
+	 
+	 System.out.println(result);
+ }
+}
