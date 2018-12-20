63b46887a8bd98bc4a93c8d131bc3872863301f7
yuhn126 <yuhn126@naver.com>
2018-12-20 23:20:53 +0900

    sec03

--- chap2/src/sec03/exam01_promotion/PromotionExample.java
+++ chap2/src/sec03/exam01_promotion/PromotionExample.java
@@ -4,20 +4,24 @@ public class PromotionExample {
 
 	public static void main(String[] args) {
 		byte byteValue = 10;
-		int intValue = byteValue;		//int <- btye
+		int intValue = byteValue;			//int(4) <- btye(1)
 		System.out.println(intValue);
 
-		char charValue = 'A';
-		intValue = charValue;			//int <- char
-		System.out.println("A의 유니코드 = " + intValue);
+		char charValue = 'B';
+		intValue = charValue;				//int <- char
+		System.out.println("B의 유니코드 = " + intValue);
 		
 		intValue = 500;
-		long longValue = intValue;		//long <- int
+		long longValue = intValue;			//long(8) <- int(4)
 		System.out.println(longValue);
 		
 		intValue = 200;
-		double doubleValue = intValue;	//double <- int
+		double doubleValue = intValue;		//double(8) <- int(4)
 		System.out.println(doubleValue);
+		
+		longValue = 10000000000L;
+		float floatValue = longValue;		//float(4)은 부동소수점방식으로 저장하기 때문에 long보다 메모리를 적게 차지함 그래서 오류X
+		System.out.println(floatValue);
 	}
 
 }
---
+++ chap2/src/sec03/exam02_casting/CastingExample.java
@@ -0,0 +1,19 @@
+package sec03.exam02_casting;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		int intValue = 44032;
+		char charValue = (char)intValue;
+		System.out.println(charValue);
+		
+		long longValue = 500;
+		intValue = (int)longValue;			// (long)8byte -> 4byte/4byte
+		System.out.println(intValue);
+		
+		double doubleValue = 3.14;
+		intValue = (int)doubleValue;
+		System.out.println(intValue);		//정수 부분만 저장됨!
+	}
+
+}
