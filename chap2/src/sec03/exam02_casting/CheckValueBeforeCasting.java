a0ce634d697f275cbb847348a406ca5b09b17a21
yuhn126 <yuhn126@naver.com>
2018-12-20 23:45:22 +0900

    Update CheckValueBeforeCasting.java

--- chap2/src/sec03/exam02_casting/CheckValueBeforeCasting.java
+++ chap2/src/sec03/exam02_casting/CheckValueBeforeCasting.java
@@ -6,10 +6,10 @@ public class CheckValueBeforeCasting {
 	
 		int i = 128;
 		
-		byte b = (byte)i;						//byte의 범위 = -128 ~ 127
+		byte b = (byte)i;				//byte의 범위 = -128 ~ 127
 		System.out.println(b +"\n");			//엉뚱한 값 저장
 		
-		if(i<-128 || i>127) {					//(i<Byte.MIN_VALUE || i>Byte.MAX_VALUE) 써도 됨 (상수)
+		if(i<-128 || i>127) {				//(i<Byte.MIN_VALUE || i>Byte.MAX_VALUE) 써도 됨 (상수)
 			System.out.println("byte타입으로 변환 할 수 없습니다.");
 			System.out.println("값을 다시 확인 해 주세요.");
 		} else {
