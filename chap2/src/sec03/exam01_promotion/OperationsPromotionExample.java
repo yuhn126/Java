5105c73230bb8eb9486105e58a25b03c05df9cf6
yuhn126 <yuhn126@naver.com>
2018-12-21 21:33:13 +0900

    Update OperationsPromotionExample.java

--- chap2/src/sec03/exam01_promotion/OperationsPromotionExample.java
+++ chap2/src/sec03/exam01_promotion/OperationsPromotionExample.java
@@ -4,11 +4,11 @@ public class OperationsPromotionExample {
 	public static void main(String[] args) {
 		int intValue = 10;
 		double doubleValue = 5.5;
-		double result = intValue + doubleValue;		//int가 double로 자동변환 됨, int로 연산하려면 double앞에 (int)추가
+		double result = intValue + doubleValue;			//int가 double로 자동변환 됨, int로 연산하려면 double앞에 (int)추가
 		
 		byte byteValue1 = 10;
 		byte byteValue2 = 20;
-		//byte byteValue3 = byteValue1 + byteValue2;	//에러남
+		//byte byteValue3 = byteValue1 + byteValue2;		//에러남
 		int intValue1 = byteValue1 + byteValue2;
 		System.out.println(intValue1);
 		
@@ -20,11 +20,11 @@ public class OperationsPromotionExample {
 		System.out.println("출력문자=" + (char)intValue2);
 		
 		int intValue3 = 10;
-		int intValue4 = intValue3/4;		//10/4=2.5
+		int intValue4 = intValue3/4;			//10/4=2.5
 		System.out.println(intValue4);
 		
 		int intValue5 = 10;
-		//int intValue6 = 10/4.0;	//에러
+		//int intValue6 = 10/4.0;			//에러
 		double doubleValue2 = intValue5 / 4.0;
 		System.out.println(doubleValue2);
 	}
