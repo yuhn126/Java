550932fdffe3f784dc94654edf16fa42a61895e0
yuhn126 <yuhn126@naver.com>
2019-01-02 18:38:21 +0900

    chap05

---
+++ chap05/src/sec06/exam01_array_bylist/ArrayCreateByValueListExample2.java
@@ -0,0 +1,26 @@
+package sec06.exam01_array_bylist;
+
+public class ArrayCreateByValueListExample2 {
+
+	public static void main(String[] args) {
+		int[] scores;
+		scores = new int[] {83,90,87};		//new연산자 배열의타입[]
+		int sum1 = 0;
+		for(int i=0; i<3; i++) {
+			sum1 += scores[i];
+		}
+		System.out.println("총합 : " + sum1);
+		
+		int sum2 = add(new int[] {83,90,87});
+		System.out.println("종합 : " + sum2);
+	}
+	
+	public static int add(int[] scores) {
+		int sum = 0;
+		for (int i=0; i<3; i++) {
+			sum += scores[i];
+		}
+		return sum;
+	}
+
+}
