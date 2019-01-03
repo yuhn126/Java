be9e19b4bf38918cad2924bfaf7bf6bb5b9d78f2
yuhn126 <yuhn126@naver.com>
2019-01-03 22:50:43 +0900

    chap05

--- chap05/src/sec06/exam04_main_argument/MainStringArrayExample.java
+++ chap05/src/sec06/exam04_main_argument/MainStringArrayExample.java
@@ -3,8 +3,8 @@ package sec06.exam04_main_argument;
 public class MainStringArrayExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("배열의 길이 : " + args.length);
+		//cmd에서 java sec06.exam04_main_argument.MainStringArrayExample abc def
 	}
 
 }
