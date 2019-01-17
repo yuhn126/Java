a8739a46c28e34d61b4407c5b85a58b39cb0694e
yuhn126 <yuhn126@naver.com>
2019-01-17 23:54:33 +0900

    chap06

---
+++ chap06/src/sec07/exam01_constructor_declaration/CarExample.java
@@ -0,0 +1,11 @@
+package sec07.exam01_constructor_declaration;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("검정", 3000);
+		//Car myCar = new Car();		기본 생성자가 없어 에러
+
+	}
+
+}
