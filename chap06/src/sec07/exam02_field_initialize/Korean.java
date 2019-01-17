5baff89576a55de8dbf51e134987b53714593664
yuhn126 <yuhn126@naver.com>
2019-01-18 00:12:11 +0900

    chap06

---
+++ chap06/src/sec07/exam02_field_initialize/Korean.java
@@ -0,0 +1,12 @@
+package sec07.exam02_field_initialize;
+
+public class Korean {
+	String nation = "대한민국";
+	String name;
+	String ssn;
+	
+	public Korean(String n, String s) {
+		name = n;
+		ssn = s;
+	}
+}
