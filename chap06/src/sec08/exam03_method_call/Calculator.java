39b3ffabc028d8e402b3d9373cd7643017a71055
yuhn126 <yuhn126@naver.com>
2019-01-21 14:30:10 +0900

    chap06

--- chap06/src/sec08/exam03_method_call/Calculator.java
+++ chap06/src/sec08/exam03_method_call/Calculator.java
@@ -7,14 +7,14 @@ public class Calculator {
 	}
 	
 	double avg(int x, int y) {
-		double sum = plus(x, y);
+		double sum = plus(x, y);		//다른 메소드 호출 시 이름으로 호출
 		double result = sum / 2;
 		return result;
 	}
 	
 	void excute() {
 		double result = avg(7, 10);
-		System.out.println("실행 결과 : " + result);
+		println("실행 결과 : " + result);
 	}
 	
 	void println(String message) {
