7e0954d53d05b9fed0fe9c996bb3f31fca3c2562
yuhn126 <yuhn126@naver.com>
2019-01-30 13:53:50 +0900

    chap06

--- chap06/src/sec12/exam03_import/mycompany/Car.java
+++ chap06/src/sec12/exam03_import/mycompany/Car.java
@@ -11,6 +11,6 @@ public class Car {
 	Engine engine = new Engine();
 	SnowTire tire1 = new SnowTire();
 	BigWidthTire tire2 = new BigWidthTire();
-	Tire tire3 = new Tire();		//어떤 패키지의 tire인지 구분하기 위해 import에 추가
+	Tire tire3 = new Tire();			//어떤 패키지의 tire인지 구분하기 위해 import에 추가
 	sec12.exam03_import.kumho.Tire tire4 = new sec12.exam03_import.kumho.Tire();	//import 필요x
 }
