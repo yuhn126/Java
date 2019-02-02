e8a3b3db0490801193ed5bd20b314fcfd9c61e92
yuhn126 <yuhn126@naver.com>
2019-02-02 21:42:33 +0900

    chap06

---
+++ chap06/src/chap06_Exercise/Exercise13/Member.java
@@ -0,0 +1,9 @@
+package chap06_Exercise.Exercise13;
+
+public class Member {
+	//회원 클래스 선언
+	String name;
+	String id;
+	String password;
+	int age;
+}
---
+++ chap06/src/chap06_Exercise/Exercise14/Member.java
@@ -0,0 +1,13 @@
+package chap06_Exercise.Exercise14;
+
+public class Member {
+	String name;
+	String id;
+	String password;
+	int age;
+	
+	Member(String name, String id){
+		this.name = name;
+		this.id = id;
+	}
+}
