8921ee706bc9c8bde491de33dbe4ff7906d9bbd6
yuhn126 <yuhn126@naver.com>
2019-02-02 21:58:05 +0900

    chap06

---
+++ chap06/src/chap06_Exercise/Exercise16/Printer.java
@@ -0,0 +1,20 @@
+package chap06_Exercise.Exercise16;
+
+public class Printer {
+	//메소드 오버로딩
+	void println(int value) {
+		System.out.println(value);
+	}
+	
+	void println(boolean value) {
+		System.out.println(value);
+	}
+	
+	void println(double value) {
+		System.out.println(value);
+	}
+	
+	void println(String value) {
+		System.out.println(value);
+	}
+}
---
+++ chap06/src/chap06_Exercise/Exercise17/PrinterExample.java
@@ -0,0 +1,14 @@
+package chap06_Exercise.Exercise17;
+
+public class PrinterExample {
+
+	public static void main(String[] args) {
+		//객체 생성하지 않고 호출하는법 = static 추가
+		Printer.println(10);
+		Printer.println(true);
+		Printer.println(5.7);
+		Printer.println("홍길동");
+
+	}
+
+}
